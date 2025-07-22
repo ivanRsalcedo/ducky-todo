90749a53d23bb73d366681f447a3b9b231eecc4a
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-22 19:56:50 -0400

    Create base style and layout for each todo item

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import Todo from './modules/todo.js';
 import ProjectHandler from './modules/projectHandler.js';
 import Project from './modules/project.js';
 import AppData from './modules/appData.js';
+import DisplayController from './modules/displayController.js';
 
 ProjectHandler.loadProjects();
 
 console.log('DUCKY TODO');
 
-window.Project = Project;
-window.ProjectHandler = ProjectHandler;
-window.AppData = AppData;
-window.Todo = Todo;
+// DisplayController.update();
+
+// window.Project = Project;
+// window.ProjectHandler = ProjectHandler;
+// window.AppData = AppData;
+// window.Todo = Todo;
--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -1,3 +1,22 @@
+import Project from "./project";
+import ProjectHandler from "./projectHandler";
+
 const DisplayController = (() => {
-    
+    const sidebar = document.querySelector('#sidebar');
+    const todoList = document.querySelector('#todo-list');
+
+    return {
+        update() {
+            for (const todo of ProjectHandler.getActiveProject().getList()){
+                const todoEl = document.createElement('div');
+                todoEl.classList.add('todo-item')
+
+                
+
+                todoList.appendChild(todoEl);
+            }
+        }
+    }
 })();
+
+export default DisplayController;
