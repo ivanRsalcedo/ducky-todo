d263d5e0d526379afd2e6634d231dc22b1f00f7f
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-22 15:59:36 -0400

    Update deleteProject() to prevent deleting the last remaining project

--- src/modules/projectHandler.js
+++ src/modules/projectHandler.js
@@ -2,6 +2,7 @@ import AppData from './appData';
 
 const ProjectHandler = (() => {
     let projects = [];
+    let activeProject = null;
     return {
         getProjects() {
             return projects;
@@ -10,12 +11,18 @@ const ProjectHandler = (() => {
             projects.push(project);
         },
         deleteProject(project) {
-            const index = projects.findIndex(predicate => predicate.id === project.id);
-            if (index !== -1) projects.splice(index, 1);
-            else console.log('Project not found in array.');
+            if (projects.length > 1) {
+                const index = projects.findIndex(predicate => predicate.id === project.id);
+                if (index !== -1) projects.splice(index, 1);
+                else console.log('Project not found in array.');
+            }
         },
         loadProjects() {
             projects = AppData.loadProjects() || [];
+            activeProject = projects[0];
+        },
+        getActiveProject() {
+            return activeProject;
         }
     }
 })();
