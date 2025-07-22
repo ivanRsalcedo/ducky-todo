a856cd637b50666c07c85d61af14fff53dbb7acb
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-22 00:15:14 -0400

    Add basic styling and layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Todo from './modules/todo.js';
+
+
+//example
+const homework = Todo('Do homework', 'Page 51 left off', '03/06/1998', '12:14 PM', 'Low' );
