ae17a54f810267173d26b29863d817c340cf0a54
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-22 21:19:16 -0400

    Fix JSON to Project type conversion and generate default project and a todo for new users

--- src/modules/appData.js
+++ src/modules/appData.js
@@ -7,7 +7,25 @@ const AppData = (() => {
         },
         loadProjects() {
             const unconverted = JSON.parse(localStorage.getItem('projects'));
-            return unconverted.map(project => Project(project.name, project.todoList));
+
+            if (!unconverted || unconverted.length === 0) {
+                const generalProject = Project('General', []);
+                generalProject.addTodo({
+                    title: 'Create your first todo!',
+                    notes: 'Change date/time due or delete a todo with the buttons on the right.',
+                    date: '',
+                    time: '',
+                    priority: ''
+                });
+                this.saveProjects([generalProject]);
+                return [generalProject];
+            }
+
+            return unconverted.map(project => {
+                const restored = Project(project.name, []);
+                project.todoList.forEach(todo => restored.addTodo(todo));
+                return restored;
+            });
         }
     }
 })();
